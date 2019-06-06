7bfd484d7c0abb5f684c2a83e7a0aea97c5ab1b6
hadi.eskandari@ymail.com <hadi.eskandari@ymail.com>
2019-06-06 20:45:42 +0430

    first commit

---
+++ SqlExenion.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using AutoMapper.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlServerExecuteStoredProcedureMapper
+{
+    public class SqlExenion
+    {
+        public static List<TEntity> ExecuteSqlMapper<TEntity>(DbContext dbContext, string StoredProcedureName, List<SqlParameter> sqlParameters = null)
+        {
+            List<TEntity> entities = null;
+            using (var command = dbContext.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = StoredProcedureName;
+                command.CommandType = CommandType.StoredProcedure;
+                if (sqlParameters != null && sqlParameters.Count > 0)
+                {
+                    foreach (var param in sqlParameters)
+                        command.Parameters.Add(param);
+                }
+                dbContext.Database.OpenConnection();
+
+                var dataReader = command.ExecuteReader();
+
+                if (dataReader.HasRows)
+                {
+                    Mapper.Reset();
+                    Mapper.Initialize(cfg =>
+                    {
+                        cfg.AddDataReaderMapping();
+                    });
+                    entities = Mapper.Map<IDataReader, List<TEntity>>(dataReader);
+                    dbContext.Database.CloseConnection();
+                }
+            }
+            return entities;
+        }
+    }
+}
